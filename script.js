38616e9e82309645ae80dcea3590532b46d36de7
Abassgold <Abasskola10@gmail.com>
2023-07-24 15:32:28 +0100

    added background chnaging button

---
+++ script.js
@@ -0,0 +1,13 @@
+const ChangeBgwhite = () => {
+    document.getElementById("changeBg").style.backgroundColor = "white"
+    console.log("abass");
+    day.style.display = "none"
+    night.style.display = "block"
+}
+
+const ChangeBgnight = () => {
+    document.getElementById("changeBg").style.backgroundColor = "white"
+    console.log("abass");
+    day.style.display = "block"
+    night.style.display = "none"
+}
