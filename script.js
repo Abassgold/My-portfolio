dcd0399f9148a09b57caf3324d370c9777a5e0b5
Abassgold <Abasskola10@gmail.com>
2023-07-25 22:14:23 +0100

    added courses

--- script.js
+++ script.js
@@ -1,13 +1,27 @@
 const ChangeBgwhite = () => {
     document.getElementById("changeBg").style.backgroundColor = "white"
+    changeBg.style.color = " #082032"
     console.log("abass");
     day.style.display = "none"
     night.style.display = "block"
+     document.getElementsByClassName("clip").style.backgroundColor = "#082032"
+     document.getElementsByClassName("clip1").style.backgroundColor = "#082032"
+     document.getElementsByClassName("clip2").style.backgroundColor = "#082032"
+     document.getElementsByClassName("clip3").style.backgroundColor = "#082032"
+     document.getElementsByClassName("clip4").style.backgroundColor = "#082032"
+     document.getElementsByClassName("clip5").style.backgroundColor = "#082032"
 }
 
 const ChangeBgnight = () => {
-    document.getElementById("changeBg").style.backgroundColor = "white"
+    changeBg.style.color = "whitesmoke"
+    document.getElementById("changeBg").style.backgroundColor = "#082032"
     console.log("abass");
     day.style.display = "block"
     night.style.display = "none"
+    document.getElementById("clip").style.backgroundColor = "white"
+    document.getElementById("clip1").style.backgroundColor = "white"
+    document.getElementById("clip2").style.backgroundColor = "white"
+    document.getElementById("clip3").style.backgroundColor = "white"
+    document.getElementById("clip4").style.backgroundColor = "white"
+    document.getElementById("clip5").style.backgroundColor = "white"
 }
